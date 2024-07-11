1f207d7e9976f592ad4590c9cbccc80c964465b6
Andreina Lima <andreinaadelima14@gmail.com>
2024-07-10 23:46:45 -0300

    Adicionado gif e estilo na página notfound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,9 +1,22 @@
+import { Container, Image } from "react-bootstrap"
+
 function NotFound() {
-  return(
+  return (
     <main>
-      <h1>Página não encontrada. 404.</h1>
+      <Container className="mt-5 mb-5 text-center">
+        <Image
+          src="https://i.gifer.com/jQ.gif"
+          fluid
+          style={{
+            border: "8px solid #ccc",
+            borderRadius: "8px",
+            margin: "0 auto"
+          }}
+        />
+        <h1 className="mt-3">Página não encontrada. Error 404.</h1>
+      </Container>
     </main>
   )
 }
 
-export default NotFound;
+export default NotFound
